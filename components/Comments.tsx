0a3a30b29db3969ba80903fe6ae6b10c65e6ee38
Shitcoder731 <shitanshu.731@gmail.com>
2024-07-22 14:45:05 +0530

    feature : add sticky comment for the docs

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -1,12 +1,32 @@
-import { Composer } from "@liveblocks/react-ui";
+import { cn } from "@/lib/utils";
+import { useIsThreadActive } from "@liveblocks/react-lexical";
+import { Composer, Thread } from "@liveblocks/react-ui";
 import { useThreads } from "@liveblocks/react/suspense";
 import React from "react";
+
+const ThreadWrapper = ({ thread }: ThreadWrapperProps) => {
+  const isActive = useIsThreadActive(thread.id);
+  return (
+    <Thread
+      thread={thread}
+      dare-state={isActive ? "active" : null}
+      className={cn(
+        "comment-thread border",
+        isActive && "!boarder-blue-500 shadow-md ",
+        thread.resolved && "opacity-40"
+      )}
+    />
+  );
+};
 
 const Comments = () => {
   const { threads } = useThreads();
   return (
     <div className="comments-container">
       <Composer className="comment-composer" />
+      {threads.map((thread) => (
+        <ThreadWrapper key={thread.id} thread={thread} />
+      ))}
     </div>
   );
 };
--- components/editor/Editor.tsx
+++ components/editor/Editor.tsx
@@ -21,6 +21,7 @@ import {
   useEditorStatus,
 } from "@liveblocks/react-lexical";
 import Loader from "../Loader";
+import Comments from "../Comments";
 
 // Catch any errors that occur during Lexical updates and log them
 // or throw them as needed. If you don't throw them, Lexical will
@@ -78,7 +79,7 @@ export function Editor({
           <LiveblocksPlugin>
             <FloatingComposer className="w-[350px]" />
             <FloatingThreads threads={threads} />
-            {/* <Comments /> */}
+            <Comments />
           </LiveblocksPlugin>
         </div>
       </div>
