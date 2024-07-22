5007e43b7351b7a09ab7a9a59bbf33a9e34eac56
Shitcoder731 <shitanshu.731@gmail.com>
2024-07-22 13:26:28 +0530

    Implement GetAllRooms func for an User.

--- lib/actions/room.actions.ts
+++ lib/actions/room.actions.ts
@@ -63,3 +63,17 @@ export const updateDocument = async (roomId: string, title: string) => {
     console.log(error);
   }
 };
+
+export const getDocuments = async ({ email }: { email: string }) => {
+  try {
+    const rooms = await liveblocks.getRooms({ userId: email });
+    // const hasAccess = Object.keys(room.usersAccesses).includes(userId);
+    // if (!hasAccess) {
+    //   throw new Error("You do not have the access  to this document");
+    // }
+
+    return parseStringify(rooms);
+  } catch (error) {
+    console.log(`Error hhappened While creating Rooms : ${error}`);
+  }
+};
